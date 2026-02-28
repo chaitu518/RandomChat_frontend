6c4b1f80bfd1d8befd60865d1854bf0805ed68df
chaitu518 <saichaitanya518@gmail.com>
2026-02-28 14:56:53 +0530

    some changes to docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,8 @@ fi
 
 BROKER_URL_ESCAPED=$(printf '%s' "$VITE_WS_BROKER_URL" | sed 's/\\/\\\\/g; s/"/\\"/g')
 
+mkdir -p /tmp/client_temp /tmp/proxy_temp /tmp/fastcgi_temp /tmp/uwsgi_temp /tmp/scgi_temp
+
 cat <<EOF > /usr/share/nginx/html/env.js
 window.__APP_CONFIG__ = {
 	VITE_WS_BROKER_URL: "${BROKER_URL_ESCAPED}"
@@ -30,6 +32,12 @@ http {
 	include       /etc/nginx/mime.types;
 	default_type  application/octet-stream;
 
+	client_body_temp_path /tmp/client_temp;
+	proxy_temp_path       /tmp/proxy_temp;
+	fastcgi_temp_path     /tmp/fastcgi_temp;
+	uwsgi_temp_path       /tmp/uwsgi_temp;
+	scgi_temp_path        /tmp/scgi_temp;
+
 	access_log /dev/stdout;
 	error_log  /dev/stderr warn;
 
@@ -39,5 +47,7 @@ http {
 	include /tmp/default.conf;
 }
 EOF
+
+nginx -t -c /tmp/nginx.conf
 
 exec nginx -c /tmp/nginx.conf -g 'daemon off;'
